c8a6b8539a00ec1c7792888c795dbe1864ede508
tomozip <dev.tomoki@gmail.com>
2018-01-27 17:00:54 +0900

    Update Header & Create ReviewBtn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
 // libs
 import React from 'react';
-import PropTypes from 'prop-types';
+
+// components
+import ReviewBtn from './ReviewBtn';
 
 // icons
 import SearchIcon from 'react-icons/lib/md/search';
 
+const header = {
+  user: {
+    name: 'tomozip',
+    image_url: 'https://placehold.jp/150x150.png',
+  }
+}
+
 const Header = props => (
   <div className="header">
-    <div className="header_search_block">
-      <input className="header_search_input" placeholder="検索" />
-      <SearchIcon className="header_search_icon" />
+    <div className="header_left_block">
+      <div className="header_search_block">
+        <input className="header_search_input" placeholder="検索" />
+        <SearchIcon className="header_search_icon" />
+      </div>
     </div>
-    <div className="header_title_block">
-      <p className="header_title">CoinNow</p>
+    <div className="c_header_logo">
+      <p className="header_logo">Artie</p>
     </div>
-    <div className="header_user_block">
-      <img className="header_user_image" src="https://placehold.jp/150x150.png" alt="profile" />
-      <p className="header_user_name">kamex</p>
+    <div className="header_right_block">
+      <div className="header_user_block">
+        <img className="header_user_image" src={header.user.image_url} alt="profile" />
+        <p className="header_user_name">{header.user.name}</p>
+      </div>
+      <div className="l_review_btn">
+        <ReviewBtn />
+      </div>
     </div>
   </div>
 );
---
+++ src/components/ReviewBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ReviewBtn = props => (
+  <button className="review_btn">
+    <span className="review_btn_text">review</span>
+  </button>
+);
+
+export default ReviewBtn;
